46c59dcb67c7cbe2d563bcb5f33b5e7d2b96b085
Or756 <87674994+Or756@users.noreply.github.com>
2023-07-07 13:20:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,39 @@
 from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
 import os
+import psycopg2
+from urllib.parse import urlparse
 import datetime
 
 app = Flask(__name__)
 
-# Retrieve the DATABASE_URL from the environment
-DATABASE_URL = os.environ['DATABASE_URL']
-
-# Configure the SQLALCHEMY_DATABASE_URI setting
-app.config['DATABASE_URI'] = DATABASE_URL
-
-db = SQLAlchemy(app)
-
-class SalesData(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    product = db.Column(db.String(50))
-    units_sold = db.Column(db.Integer)
-    revenue = db.Column(db.Float)
-    cost = db.Column(db.Float)
-    profit = db.Column(db.Float)
-    date = db.Column(db.Date)
-
-    def __init__(self, product, units_sold, revenue, cost, profit, date):
-        self.product = product
-        self.units_sold = units_sold
-        self.revenue = revenue
-        self.cost = cost
-        self.profit = profit
-        self.date = date
+def get_conn():
+    result = urlparse(os.getenv("DATABASE_URL"))
+    database = result.path[1:]
+    hostname = result.hostname
+    conn = psycopg2.connect(
+        database = database,
+        host = hostname
+    )
+    return conn
 
 @app.route('/webhook', methods=['POST'])
 def respond():
     data = request.json
-    new_data = SalesData(
-        data['Product'], 
-        data['Units sold'], 
-        data['Revenue'], 
-        data['Cost'], 
-        data['Profit'], 
-        datetime.datetime.strptime(data['Date'], '%m/%d/%Y')  # date string should be converted to datetime object
-    )
-    db.session.add(new_data)
-    db.session.commit()
+    conn = get_conn()
+    cur = conn.cursor()
+
+    date = datetime.datetime.strptime(data['date'], '%m/%d/%Y').date()  # date string should be converted to date object
+
+    cur.execute("""
+        INSERT INTO salesdata (product, units_sold, revenue, cost, profit, date)
+        VALUES (%s, %s, %s, %s, %s, %s)
+    """, (data['product'], data['units_sold'], data['revenue'], data['cost'], data['profit'], date))
+
+    conn.commit()
+    cur.close()
+    conn.close()
 
     return {'message': 'Data received and stored.'}, 201
 
-# Add this at the end of your file to create tables in the database as per the models defined
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+if __name__ == '__main__':
     app.run(debug=True)
